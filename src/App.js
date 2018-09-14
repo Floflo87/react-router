068e3468cf10234d611d1498137ae0e10d00c920
Florian Müller <flointex@gmail.com>
2018-09-14 12:46:54 +0200

    Creating Routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
+const NewRoute = () => {
+    return <div>Hi</div>;
+};
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Route path="/new" component={NewRoute} />
+                    <Route path="/" component={Home} exact />
+                    <Route path="/contact" component={Contact} />
+                    <Route path="/about" component={About} />
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
